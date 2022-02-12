68aced3915fe3ce33522e720d830321a00028eb6
luanlipperthoffmann <luan.lippert91@rede.ulbra.br>
2022-02-12 15:39:26 -0300

    Início dos estudos de python

---
+++ hello.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+print ("Olá, Mundo!")
+
+#Exemplo de comentário em python em uma linha
+
+"""Comentário de  
+muitas linhas
+em Python"""
+ 
+#Tipo de operações matemáticas em python:
+print(2+2) #soma
+print(2-2) #subtração
+print(2*2) #multiplicação
+print(2/2) #divisão
+print(2**2) #exponenciação
+print(10%3) #módulos, irá retornar o resto de um divisáo de numeros inteiros
+ 
